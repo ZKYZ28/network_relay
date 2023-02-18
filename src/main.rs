9908fa7dfd5f536b6803e1e599235f1ceb7c1d40
Anthony UCCI <a.ucci@student.helmo.be>
2023-02-18 12:42:56 +0100

    relay mise en place ecoute reseau ECHO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{UdpSocket, IpAddr, Ipv4Addr, SocketAddr};
+use std::str::FromStr;
+
+fn main() -> std::io::Result<()> {
+    // Set the multicast address and port to listen on
+    let multicast_addr = "224.1.1.255";
+    let port = 23106;
+
+    // Create a UDP socket bound to the multicast address and port
+    let socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port))?;
+    socket.join_multicast_v4(&Ipv4Addr::from_str(multicast_addr).unwrap(), &Ipv4Addr::new(0, 0, 0, 0))?;
+
+    // Listen for multicast packets
+    let mut buf = [0; 1024];
+    loop {
+        let (size, src) = socket.recv_from(&mut buf)?;
+        println!("Received {} bytes from {}", size, src);
+        println!("{}", String::from_utf8_lossy(&buf[..size]));
+    }
 }
---
+++ target/debug/build/memchr-dc1fa73c6f6d50c8/build_script_build-dc1fa73c6f6d50c8.d
@@ -0,0 +1,5 @@
+C:\Users\uccia\OneDrive - student.helmo.be\Documents\Informatique de gestion\Bloc 2\Q2\Réseau\Labo\Projet\Murmur\murmur_relay\murmumr-relay\target\debug\build\memchr-dc1fa73c6f6d50c8\build_script_build-dc1fa73c6f6d50c8.exe: C:\Users\uccia\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+C:\Users\uccia\OneDrive - student.helmo.be\Documents\Informatique de gestion\Bloc 2\Q2\Réseau\Labo\Projet\Murmur\murmur_relay\murmumr-relay\target\debug\build\memchr-dc1fa73c6f6d50c8\build_script_build-dc1fa73c6f6d50c8.d: C:\Users\uccia\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs
+
+C:\Users\uccia\.cargo\registry\src\github.com-1ecc6299db9ec823\memchr-2.5.0\build.rs:
