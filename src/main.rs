f35b781b3113207025bb153a7222b458b8ca4a63
Giuseppe Franchina <l.loiseau@student.helmo.be>
2023-03-10 01:20:56 +0100

    Comments in main

--- src/main.rs
+++ src/main.rs
@@ -14,47 +14,54 @@ use crate::server_runnable::ServerRunnable;
 
 static PORT: u16 = 23106;
 static MULTICAST_IP: &str = "224.1.1.255";
+static JSON_PATH : &str = "src/ressources/relayConfig.json";
 
 fn main() {
 
     //Création des map
     let server_map: Arc<Mutex<HashMap<String, TcpStream>>> = Arc::new(Mutex::new(HashMap::new()));
-    let server_aes_map = config_reader::read_config("src/ressources/relayConfig.json").unwrap();
+    let server_aes_map = config_reader::read_config(JSON_PATH).unwrap();
 
 
-    println!("-----Démarrage de l'écoute multicast.");
-    receive_multicast(server_map.clone(), server_aes_map).expect("An error occurred while listening in multicast.");
+    println!("-----Démarrage de l'écoute multicast sur l'IP {} et le port {}", PORT, MULTICAST_IP);
+    receive_multicast(server_map.clone(), server_aes_map).expect("Une erreur est survenue lors de l'écoute en multicast.");
 }
 
-/**
- * Méthode qui écoute en boucle pour les echo et qui ajoute dans servermap quand une connection est éffectuée
- */
+///
+/// La méthode receive_multicast écoute en permanence les appels multicast sur l'adresse IP et le port définis.
+/// Si un serveur s'annonce mais qu'il ne fait pas partie de la liste des serveurs partageant une clé AES avec le relais, il sera ignoré.
+/// Sinon, une connexion TCP sera créée sur ce serveur à l'aide des informations annoncées, et un nouveau thread sera créé pour la réception et l'envoi de messages vers ou depuis ce serveur en TCP.
+/// Cette méthode doit être appelée une seule fois au lancement du programme et fonctionne même si d'autres threads ont été créés auparavant.
+///
 fn receive_multicast(server_map: Arc<Mutex<HashMap<String, TcpStream>>>, aes_map: HashMap<String, String>) -> Result<(), std::io::Error> {
-    let socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), PORT))?;    //Création d'un socket UDP et le lie à toutes les interfaces locales en écoutant le port spécifié
-    socket.join_multicast_v4(&Ipv4Addr::from_str(MULTICAST_IP).unwrap(), &Ipv4Addr::new(0, 0, 0, 0))?;  //Permet de joindre un groupe de diffusion multicast IPv4 en utilisant l'adresse IP multicast spécifiée (stockée dans la variable MULTICAST_IP) et en écoutant sur toutes les interfaces locales.
+
+    let socket = UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), PORT))?;            //Création d'un socket UDP et le lie à toutes les interfaces locales en écoutant le port spécifié
+    socket.join_multicast_v4(&Ipv4Addr::from_str(MULTICAST_IP).unwrap(), &Ipv4Addr::new(0, 0, 0, 0))?;          //Permet de joindre un groupe de diffusion multicast IPv4 en utilisant l'adresse IP multicast spécifiée (stockée dans la variable MULTICAST_IP) et en écoutant sur toutes les interfaces locales.
 
     loop {
-        let mut buf = [0u8; 1024];                                                       //Crée un tableau mutable de 1024 éléments de type u8, initialisés à zéro, pour stocker des données qui seront lues à partir d'un socket.
+        let mut buf = [0u8; 1024];                                                                                                    //Crée un tableau mutable de 1024 éléments de type u8 (Entier non signé de 8 bits), initialisés à zéro, pour stocker des données qui seront lues à partir d'un socket.
 
-        let (size, _) = socket.recv_from(&mut buf)?;                                        //Lit les données reçues sur un socket UDP
+        let (size, _) = socket.recv_from(&mut buf)?;                                                                                     //Lit les données reçues sur un socket UDP, bloquant si rien reçu
 
-        let echo_message = String::from_utf8_lossy(&buf[..size]);                     //Convertit le tableau de byte en String
+        let echo_message = String::from_utf8_lossy(&buf[..size]);                                                                  //Convertit un vecteur d'octets en une chaîne de caractères
 
-        if let Some(map) = Protocol::get_echo_map(&echo_message) {      //Via la méthode get_echo_map de Protocol, recupère les info du echo
-            let domain = map.get("domain").unwrap().to_string();                        //Domain annoncé dans le echo
-            let port = map.get("port").unwrap();                                       //Port annoncé dans le echo
+        if let Some(map) = Protocol::get_echo_map(&echo_message) {                                                     //Via la méthode get_echo_map de Protocol, recupère les info du echo à l'aide d'une map
+            let domain = map.get("domain").unwrap().to_string();                                                                      //Domain annoncé dans le echo
+            let port = map.get("port").unwrap();                                                                                     //Port annoncé dans le echo
 
             println!("ECHO received from server {} on port {}.", domain, port);
-            println!(" VALEUR &domain : {}", &domain);
-            if aes_map.contains_key(&domain) {                                                            //Vérification que le serveur partage bien une clé AES
-                let unicast_socket = TcpStream::connect(format!("{}:{}", domain, port))?;
-                let mut map = server_map.lock().unwrap();
-                map.insert(domain.clone(), unicast_socket.try_clone().expect("Problème lors du clonage du unicast socket"));  //Ajout du socket dans la map de serveur connecté
-                println!("Connection établie avec le serveur {}.", domain.clone());
 
-                let aes_key = aes_map.get(&domain).unwrap().to_string();                           //Récupération de la clé AES stockée
+            if aes_map.contains_key(&domain) {                                                                                               //Vérification que le serveur partage bien une clé AES avec le serveur ennoncé
+                let unicast_socket = TcpStream::connect(format!("{}:{}", domain, port))?;                                        //Connection au ServeurSocket du serveur
 
-                let server_map_clone = server_map.clone();                            //Lancement du thread serveur
+                let mut map = server_map.lock().unwrap();                                                                //Crée un verrou sur la map grâce a lock pour garantir qu'un seul thread peut y accéder à la fois
+                map.insert(domain.clone(), unicast_socket.try_clone().expect("Problème lors du clonage du unicast socket"));         //Ajout du socket dans la map de serveur connecté
+
+                println!("Connection établie avec le serveur {} !", domain.clone());
+
+                let aes_key = aes_map.get(&domain).unwrap().to_string();                                                              //Récupération de la clé AES stockée
+
+                let server_map_clone = server_map.clone();                                                               //Sans l'utilisation du mot clé move, ces variables ne seraient pas disponibles dans le nouveau thread, car Rust garantit que chaque variable ne peut avoir qu'un propriétaire à la fois. En utilisant le mot clé move, Rust transfère la propriété de server_map_clone et aes_key à la closure du thread, permettant ainsi leur utilisation dans ce nouveau contexte.
                 thread::spawn(move || {
                     let server_runnable = ServerRunnable::new(server_map_clone, aes_key);
                     server_runnable.handle_client(&unicast_socket);
@@ -65,3 +72,22 @@ fn receive_multicast(server_map: Arc<Mutex<HashMap<String, TcpStream>>>, aes_map
         }
     }
 }
+
+
+
+
+
+
+
+///
+/// Dans ce contexte, Arc et Mutex sont utilisés pour partager des données entre plusieurs threads de manière sûre et efficace.
+///
+/// Arc signifie "Atomic Reference Counting" et est utilisé pour créer une référence partagée entre plusieurs threads.
+/// Dans cet exemple, server_map est une référence partagée à un HashMap qui stocke des connexions TCP.
+///
+/// L'accès simultané à HashMap par plusieurs threads pourrait causer des problèmes de concurrence. C'est là que Mutex intervient.
+/// Le Mutex permet de verrouiller l'accès à une ressource partagée entre plusieurs threads.
+/// Cela signifie que seul un thread peut accéder à la ressource verrouillée à un moment donné.
+///
+/// Ainsi, en combinant Arc et Mutex, nous avons une référence partagée à un HashMap qui peut être utilisée par plusieurs threads simultanément, en toute sécurité.
+///
